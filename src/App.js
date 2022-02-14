9419c64072cc7d26ba54952a7d5d4a780502f1b8
Rodrigo Brentano <rodrigobjj24@gmail.com>
2022-02-14 15:04:01 -0300

    Version 1.14

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Home from "./components/Home/Home";
-import Search from "./components/Search/Search";
-import Details from "./components/Details/Details";
+import Home from "./screens/Home/Home";
+import Search from "./screens/Search/Search";
+import Details from "./screens/Details/Details";
 import Styles from "./Styles";
 
 const Tab = createMaterialTopTabNavigator();
